176ebf2b03185fa7aa79f2f786698457d1dd1d88
sunsetlover36 <sunsetloverrr36@gmail.com>
2023-06-30 04:09:20 +0300

    Add loader frames and change required USD amounts

--- constants/index.js
+++ constants/index.js
@@ -5,10 +5,12 @@ export const NATIVE_TOKEN_ADDRESS =
 
 export const SLIPPAGE_MULTIPLIER = 1 - config.slippage / 100;
 
-export const REQUIRED_USD_AMOUNT_FOR_MAIN_ACCOUNT = 2;
-export const REQUIRED_USD_AMOUNT_FOR_SECONDARY_ACCOUNT = 1.25;
+export const REQUIRED_STABLE_USD_AMOUNT_FOR_MAIN_ACCOUNT = 0.5;
+export const REQUIRED_NATIVE_USD_AMOUNT_FOR_ACCOUNT = 2;
 
 export const APPROVAL_AMOUNT_MULTIPLIER = 10;
+
+export const LOADER_FRAMES = ["-", "\\", "|", "/"];
 
 export { chainToColor } from "./chainToColor";
 export { moduleToColor } from "./moduleToColor";
