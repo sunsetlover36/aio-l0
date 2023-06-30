0206c890f2aeb4f142c86593e233ae3cb634031b
sunsetlover36 <sunsetloverrr36@gmail.com>
2023-06-30 03:04:23 +0300

    Add custom gas params

--- utils/sendTx.js
+++ utils/sendTx.js
@@ -1,14 +1,21 @@
-export const sendTx = async (wallet, txData) => {
+export const sendTx = async (wallet, txData, gasParams) => {
   let populatedTx;
 
   try {
+    if (gasParams) {
+      throw new Error();
+    }
+
     const feeData = await wallet.provider.getFeeData();
     populatedTx = await wallet.populateTransaction({
       ...txData,
       gasPrice: feeData.gasPrice,
     });
   } catch {
-    populatedTx = await wallet.populateTransaction(txData);
+    populatedTx = await wallet.populateTransaction({
+      ...txData,
+      ...(gasParams || {}),
+    });
   }
   const tx = await wallet.sendTransaction(populatedTx);
   await wallet.provider.waitForTransaction(tx.hash);
